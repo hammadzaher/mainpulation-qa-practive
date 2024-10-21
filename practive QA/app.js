3ab863abb88a4edc94bc4add4d08a1bc8105976c
hammadzaher <zaheerhammad62@gmail.com>
2024-10-21 23:12:39 +0500

    practive QA

---
+++ practive QA/app.js
@@ -0,0 +1,78 @@
+let question1 = ` Duniya ka sabse lamba pehad kaun sa hai? <button onclick="showanswer1()">Show Answer</button>`
+document.querySelector("#para1").innerHTML = question1;
+
+
+function showanswer1() {
+    document.querySelector("#para1").innerHTML = `<p class = "hhh"> Duniya ka sabse lamba pehad Mount Everest hai, jo Himalayas range mein Nepal aur China ke border par waqia hai. Iski height 8,848 meters (29,029 feet) hai. <button onclick="showanswer1d()">Hide Answer</button> </p>`
+}
+
+function showanswer1d() {
+    document.querySelector("#para1").innerHTML = question1
+}
+
+
+// /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+
+let question2 = `Aik saal mein kitnay din hotay hain? <button onclick="showanswer2()">Show Answer</button>`
+document.querySelector("#para2").innerHTML = question2;
+
+
+function showanswer2() {
+    document.querySelector("#para2").innerHTML = `<p class = "hhh"> Aik saal mein aam tor par 365 din hotay hain. Agar leap year ho, toh 366 din hotay hain. <button onclick="showanswer2d()">Hide Answer</button> </p>`
+}
+
+function showanswer2d() {
+    document.querySelector("#para2").innerHTML = question2
+}
+
+
+// /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+
+let question3 = ` Sab se zyada bolay janay wali language kaun si hai? <button onclick="showanswer3()">Show Answer</button>`
+document.querySelector("#para3").innerHTML = question3;
+
+
+function showanswer3() {
+    document.querySelector("#para3").innerHTML = `<p class = "hhh"> Duniya mein sab se zyada bolay janay wali language English hai. Is ke baad Mandarin Chinese aur Hindi aati hain. <button onclick="showanswer3d()">Hide Answer</button> </p>`
+}   
+
+function showanswer3d() {
+    document.querySelector("#para3").innerHTML = question3
+}
+
+
+// /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+
+let question4 = ` Sab se bari mammal kaun si hai? <button onclick="showanswer4()">Show Answer</button>`
+document.querySelector("#para4").innerHTML = question4;
+
+
+function showanswer4() {
+    document.querySelector("#para4").innerHTML = `<p class = "hhh"> Duniya ki sab se bari mammal Blue Whale hai. Yeh samundar mein paayi jaati hai aur iska wazan 200 ton tak ho sakta hai. <button onclick="showanswer4d()">Hide Answer</button> </P>`
+}
+
+function showanswer4d() {
+    document.querySelector("#para4").innerHTML = question4
+}
+
+
+// /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+
+let question5 = ` Pehla computer kisne banaya tha? <button onclick="showanswer5()">Show Answer</button>`
+document.querySelector("#para5").innerHTML = question5;
+
+
+function showanswer5() {
+    document.querySelector("#para5").innerHTML = `<p class = "hhh"> Pehla programmable computer Charles Babbage ne design kiya tha, jise "father of the computer" kaha jata hai. <button onclick="showanswer5d()">Hide Answer</button> </p>`
+}
+
+function showanswer5d() {
+    document.querySelector("#para5").innerHTML = question5
+}
+
+
+// /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
